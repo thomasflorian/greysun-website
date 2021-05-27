c162edb6993dda3043c05aac54281f492e5d7792
Thomas Florian <thomas.florian@marquette.edu>
2021-05-26 21:38:27 -0500

    slightly more appealing dashboard ui

--- src/components/Content/DashboardContent.js
+++ src/components/Content/DashboardContent.js
@@ -27,14 +27,14 @@ function DashboardContent(props) {
     useEffect(() => {window.scrollTo(0,0)}, [])
     // Loads blows
     useEffect(getBlows, [currUser.uid])
-    const blowComponents = blows.map((blow, i) => (<li key={i}>{`BAC was ${blow.bac} at ${blow.timestamp.toDate().getHours()}:${blow.timestamp.toDate().getMinutes()} on ${blow.timestamp.toDate().toDateString()}`}</li>))
+    const blowComponents = blows.reverse().map((blow, i) => (<Box style={{display:"flex", justifyContent:"center", width:"100%"}} key={i}><div style={{ background: i === 0 ? "var(--primary":"var(--secondary-light", width:"20rem", padding: i===0 ? "0.8rem":"0.5rem", margin:"0.2rem", borderRadius:"1rem"}}><Typography variant={i === 0 ? "body2":"caption"} >{`BAC was ${blow.bac} at ${(blow.timestamp.toDate().getHours() + 11) % 12 + 1}:${String(blow.timestamp.toDate().getMinutes()).padStart(2, '0')} ${blow.timestamp.toDate().getHours() >= 12 ? "PM":"AM" } on ${blow.timestamp.toDate().toDateString()}`}</Typography></div></Box>))
 
     return (
         <Container style={{height:"600px", display:"flex", flexWrap:"wrap", justifyContent:"center", textAlign:"center", alignContent:"start"}}>
             <Typography variant="h1" style={{width:"100%"}}>Your Dashboard</Typography>
             <Typography variant="h6">{currUser ? `Welcome ${currUser.email}` : "No user is logged in"}</Typography>
-            <Box style={{display:"flex", justifyContent:"center", width:"100%"}}>
-                {loading ? <h1>Loading</h1> : <ul>{blowComponents}</ul>}
+            <Box style={{display:"flex", flexWrap:"wrap", justifyContent:"center", width:"100%", marginTop:"2rem"}}>
+                {loading ? <h1>Loading</h1> : blowComponents}
             </Box>
         </Container>
     )
