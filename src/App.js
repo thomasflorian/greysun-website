847f1bfe7d9dd5dcbdc90d930baf237df5280ca4
Thomas Florian <thomas.florian@marquette.edu>
2022-02-20 16:52:47 -0600

    fixed build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom'
-import {ThemeProvider, createMuiTheme, responsiveFontSizes} from '@material-ui/core/styles'
-import NavComponent from './components/NavComponent'
-import ContentComponent from './components/ContentComponent'
-import FooterComponent from './components/FooterComponent'
+// import {BrowserRouter as Router} from 'react-router-dom'
+// import {ThemeProvider, createMuiTheme, responsiveFontSizes} from '@material-ui/core/styles'
+// import NavComponent from './components/NavComponent'
+// import ContentComponent from './components/ContentComponent'
+// import FooterComponent from './components/FooterComponent'
 import React, { useState } from 'react'
-import { AuthProvider } from './contexts/AuthContext';
+// import { AuthProvider } from './contexts/AuthContext';
 
-const theme = responsiveFontSizes(createMuiTheme({
-  typography: {
-    fontFamily: "Montserrat"
-  },
-  palette: {
-    primary: {
-      main: "#df731a"
-    },
-    secondary: {
-      main: "#111111"
-    }
-  }
-}))
+// const theme = responsiveFontSizes(createMuiTheme({
+//   typography: {
+//     fontFamily: "Montserrat"
+//   },
+//   palette: {
+//     primary: {
+//       main: "#df731a"
+//     },
+//     secondary: {
+//       main: "#111111"
+//     }
+//   }
+// }))
 
 function App() {
 
